540871222f6a497ca558545f65df6a926ed4985f
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-09-13 22:50:18 -0400

    clean up

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+
 const mysql = require('mysql2')
 const connection = require('./config/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const { handlers } = require("./lib");
 
 const initializeApp = async () => {
   const openingChoices = [
@@ -28,52 +28,228 @@ const initializeApp = async () => {
   switch (answers.opening) {
     case openingChoices[0]:
       console.log("Goodbye!")
-      return handlers.end()
+  
 
-    case openingChoices[1]:
-      await handlers.view.departments()
-      break
+    .then(function(val) {
+      switch (val.choice) {
+          case "View All Employees?":
+            viewAllEmployees();
+          break;
+  
+        case "View All Employee's By Roles?":
+            viewAllRoles();
+          break;
+        case "View all Emplyees By Deparments":
+            viewAllDepartments();
+          break;
+        
+        case "Add Employee?":
+              addEmployee();
+            break;
 
-    case openingChoices[2]:
-      await handlers.view.roles()
-      break
+        case "Update Employee":
+              updateEmployee();
+            break;
+    
+          case "Add Role?":
+              addRole();
+            break;
+    
+          case "Add Department?":
+              addDepartment();
+            break;
+  
+          }
+  })
+}
+function viewAllEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+})
+}
+function viewAllRoles() {
+connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+function(err, res) {
+if (err) throw err
+console.table(res)
+startPrompt()
+})
+}
+function viewAllDepartments() {
+connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+function(err, res) {
+  if (err) throw err
+  console.table(res)
+  startPrompt()
+})
+}
 
-    case openingChoices[3]:
-      await handlers.view.employees()
-      break
+var roleArr = [];
+function selectRole() {
+connection.query("SELECT * FROM role", function(err, res) {
+  if (err) throw err
+  for (var i = 0; i < res.length; i++) {
+    roleArr.push(res[i].title);
+  }
 
-    case openingChoices[4]:
-      await handlers.add.department()
-      break
+})
+return roleArr;
+}
+var managersArr = [];
+function selectManager() {
+connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+  if (err) throw err
+  for (var i = 0; i < res.length; i++) {
+    managersArr.push(res[i].first_name);
+  }
 
-    case openingChoices[5]:
-      await handlers.add.role()
-      break
+})
+return managersArr;
+}
+function addEmployee() { 
+  inquirer.prompt([
+      {
+        name: "firstname",
+        type: "input",
+        message: "Enter their first name "
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "Enter their last name "
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is their role? ",
+        choices: selectRole()
+      },
+      {
+          name: "choice",
+          type: "rawlist",
+          message: "Whats their managers name?",
+          choices: selectManager()
+      }
+  ]).then(function (val) {
+    var roleId = selectRole().indexOf(val.role) + 1
+    var managerId = selectManager().indexOf(val.choice) + 1
+    connection.query("INSERT INTO employee SET ?", 
+    {
+        first_name: val.firstName,
+        last_name: val.lastName,
+        manager_id: managerId,
+        role_id: roleId
+        
+    }, function(err){
+        if (err) throw err
+        console.table(val)
+        startPrompt()
+    })
 
-    case openingChoices[6]:
-      await handlers.add.employee()
-      break
+})
+}
+function updateEmployee() {
+  connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+  // console.log(res)
+   if (err) throw err
+   console.log(res)
+  inquirer.prompt([
+        {
+          name: "lastName",
+          type: "rawlist",
+          choices: function() {
+            var lastName = [];
+            for (var i = 0; i < res.length; i++) {
+              lastName.push(res[i].last_name);
+            }
+            return lastName;
+          },
+          message: "What is the Employee's last name? ",
+        },
+        {
+          name: "role",
+          type: "rawlist",
+          message: "What is the Employees new title? ",
+          choices: selectRole()
+        },
+    ]).then(function(val) {
+      var roleId = selectRole().indexOf(val.role) + 1
+      connection.query("UPDATE employee SET WHERE ?", 
+      {
+        last_name: val.lastName
+         
+      }, 
+      {
+        role_id: roleId
+         
+      }, 
+      function(err){
+          if (err) throw err
+          console.table(val)
+          startPrompt()
+      })
 
-    case openingChoices[7]:
-      await handlers.update.role()
-      break
+  });
+});
 
-    case openingChoices[8]:
-      await handlers.update.employee()
-      break
+}
+function addRole() { 
+connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+  inquirer.prompt([
+      {
+        name: "Title",
+        type: "input",
+        message: "What is the roles Title?"
+      },
+      {
+        name: "Salary",
+        type: "input",
+        message: "What is the Salary?"
 
-    case openingChoices[9]:
-      await handlers.delete.department()
-      break
+      } 
+  ]).then(function(res) {
+      connection.query(
+          "INSERT INTO role SET ?",
+          {
+            title: res.Title,
+            salary: res.Salary,
+          },
+          function(err) {
+              if (err) throw err
+              console.table(res);
+              startPrompt();
+          }
+      )
 
-    case openingChoices[10]:
-      await handlers.delete.role()
-      break
+  });
+});
+}
+function addDepartment() { 
 
-    case openingChoices[11]:
-      await handlers.delete.employee()
-      break
-  };
+  inquirer.prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What Department would you like to add?"
+      }
+  ]).then(function(res) {
+      var query = connection.query(
+          "INSERT INTO department SET ? ",
+          {
+            name: res.name
+          
+          },
+          function(err) {
+              if (err) throw err
+              console.table(res);
+              startPrompt();
+          }
+      )
+  })
+}
 
   return initializeApp()
 }
